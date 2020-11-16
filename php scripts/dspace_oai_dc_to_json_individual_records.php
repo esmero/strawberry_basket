145245def41ab485c826aba257b1e3932ba19a0a
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2020-11-16 16:04:05 -0500

    Create dspace_oai_dc_to_json_individual_records.php

---
+++ php scripts/dspace_oai_dc_to_json_individual_records.php
@@ -0,0 +1,3 @@
+<?PHP
+// This depends on a call like this
+// docker run -ti esmero/catmandu bash -c "catmandu convert OAI --url https://dome.mit.edu/oai/request --metadataPrefix oai_dc --set col_1721.3_74804 --handler struct" > dim_connick.json
