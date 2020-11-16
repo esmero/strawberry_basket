6d865f0998e5214928bd7a2dc7f2c49c06194199
Allison Lund <67483756+alliomeria@users.noreply.github.com>
2020-11-16 16:07:07 -0500

    Update dspace_oai_dc_to_json_individual_records.php
    The initial PHP version that Diego created for the oai_dc for the mit dome records.

--- php scripts/dspace_oai_dc_to_json_individual_records.php
+++ php scripts/dspace_oai_dc_to_json_individual_records.php
@@ -1,3 +1,37 @@
 <?PHP
 // This depends on a call like this
 // docker run -ti esmero/catmandu bash -c "catmandu convert OAI --url https://dome.mit.edu/oai/request --metadataPrefix oai_dc --set col_1721.3_74804 --handler struct" > dim_connick.json
+$arguments = getopt("i:o:");
+
+// -i is path our JSON file
+// -o is the output path for files (base one)
+// php dspace_oai_pdf.php -i "connick_coll_job_files.json" -o "mitJSONfiles/"
+
+$srcunix = preg_replace('/\s/i', '\ ',$arguments['i']);
+$destunix = preg_replace('/\s/i', '\ ',$arguments['o']);
+$fileurl = 'http://dome.mit.edu/bitstream/';
+
+$json = file_get_contents($srcunix);
+$oaistream = json_decode($json,true);
+$numberofrecords = 0;
+foreach($oaistream as $record) {
+  if (isset($record["identifier"])) {
+    $url = NULL;
+    foreach($record["identifier"] as $key => $id) {
+      // This is weird but that is how Dspace builds this
+      // http://hdl.handle.net/
+      if ($key == 0) {
+        $itemid = $id;
+      }
+    }
+	$record["dspace_type"] = $record["type"]; 
+	$record["type"] = "Photograph";
+	$record["label"] = $record["title"][0]; 
+	unset($record["_resumption"]);
+	$recordinjson = json_encode($record);
+	file_put_contents($destunix . "/" . $itemid . ".json", $recordinjson);
+	if ($numberofrecords == 2000){
+		break;
+	}
+  }
+}
